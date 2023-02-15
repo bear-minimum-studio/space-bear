19b18028e2b7393a8f4afe6c6fcc149f7583d294
Pierre Poupin <pierrep@bam.tech>
2023-02-15 15:32:04 +0100

    feat: add rotation

--- Player.gd
+++ Player.gd
@@ -2,13 +2,19 @@ extends CharacterBody2D
 
 
 const ACCEL = 60.0
+const ROTATION_SPEED = 0.005
 
 func _physics_process(delta):
+	var rotation_intensity = Input.get_axis("ui_left", "ui_right")
+	self.rotation += rotation_intensity * ROTATION_SPEED
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("decelerate", "accelerate")
-	if direction:
-		velocity.x += delta * direction * ACCEL
+	var intensity = Input.get_axis("decelerate", "accelerate")
+	if intensity:
+		var v = delta * intensity * ACCEL
+		velocity.x += v * sin(self.rotation)
+		velocity.y += -v * cos(self.rotation)
 
 	# TODO max speed
 
