dab01b8634cea66736ee3bcf25bcbb74ab1882bc
Pierre Poupin <pierrep@bam.tech>
2023-02-15 16:14:56 +0100

    chore: add comment

--- Bullet.gd
+++ Bullet.gd
@@ -2,12 +2,6 @@ extends Sprite2D
 
 const SPEED = 500
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-#	translate(delta * self.rotation * SPEED)
+	# TODO: we should probably use physics nodes?
 	translate(Vector2(delta * SPEED * cos(self.rotation), delta * SPEED * sin(self.rotation)))
