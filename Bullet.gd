d211c23d76e4005e241dc7b2c149dd762198cf4d
Pierre Poupin <pierrep@bam.tech>
2023-02-15 16:29:54 +0100

    feat: add dumb collision between bullet and enemy

--- Bullet.gd
+++ Bullet.gd
@@ -1,7 +1,11 @@
-extends Sprite2D
+extends Node2D
 
 const SPEED = 500
 
 func _process(delta):
 	# TODO: we should probably use physics nodes?
 	translate(Vector2(delta * SPEED * cos(self.rotation), delta * SPEED * sin(self.rotation)))
+
+
+func _on_area_2d_body_entered(body):
+	print("hit!")
