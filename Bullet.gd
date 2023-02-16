156cc8b5b70c3a07b623979d8c1a2f76a06cc9c7
legodaig <gael.le-godais@gipsa-lab.grenoble-inp.fr>
2023-02-16 12:59:51 +0100

    feat: add bullet sound

--- Bullet.gd
+++ Bullet.gd
@@ -6,6 +6,9 @@ func _process(delta):
 	# TODO: we should probably use physics nodes?
 	translate(Vector2(delta * SPEED * cos(self.rotation), delta * SPEED * sin(self.rotation)))
 
+func _ready():
+	$SFX.set_pitch_scale(randf_range(0.95,1.1))
+	$SFX.play(0)
 
 func _on_area_2d_body_entered(body):
 	if (body.has_method("on_hit")):
