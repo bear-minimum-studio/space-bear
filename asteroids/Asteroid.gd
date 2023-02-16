a6a4bee7a9b6f3738e5a559f8a89885e357b60b1
Pierre Poupin <pierrep@bam.tech>
2023-02-16 12:22:06 +0100

    feat: make asteroids rotate at different speeds

--- asteroids/Asteroid.gd
+++ asteroids/Asteroid.gd
@@ -7,11 +7,17 @@ var sprites = [
 	preload("res://asteroids/asteroide2.png"),
 	preload("res://asteroids/asteroide3.png")
 ]
-@onready var sprite = $Sprite
+
+@onready var sprite = $SpriteContainer/Sprite
+@onready var sprite_container = $SpriteContainer
+@onready var animation_player = $AnimationPlayer
 
 func _ready():
 	var rand_index = randi() % sprites.size()
 	sprite.texture = sprites[rand_index]
+	
+	sprite_container.rotation = randf_range(0, 2*PI)
+	animation_player.speed_scale = randf_range(0.2, 2)
 
 func on_hit():
 	health -= 1
