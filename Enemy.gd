f6c42c02624441c7c9c85484356c1ba4653a38da
Pierre Poupin <pierrep@bam.tech>
2023-02-16 12:07:12 +0100

    feat: add asteroids

--- Enemy.gd
+++ Enemy.gd
@@ -2,6 +2,17 @@ extends CharacterBody2D
 
 var health = 10
 
+var sprites = [
+	preload("res://asteroids/asteroide1.png"),
+	preload("res://asteroids/asteroide2.png"),
+	preload("res://asteroids/asteroide3.png")
+]
+@onready var sprite = $Sprite
+
+func _ready():
+	var rand_index = randi() % sprites.size()
+	sprite.texture = sprites[rand_index]
+
 func on_hit():
 	health -= 1
 
