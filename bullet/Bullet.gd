aba6c9de743967df3870976ae1c34a2df730bcfb
legodaig <gael.le-godais@gipsa-lab.grenoble-inp.fr>
2023-02-17 00:33:31 +0100

    refactor: use AudioStreamRandomizer for bullet sounds

--- bullet/Bullet.gd
+++ bullet/Bullet.gd
@@ -3,8 +3,7 @@ extends Area2D
 var velocity = Vector2.ZERO;
 
 func _ready():
-	$SFX.set_pitch_scale(randf_range(0.95,1.1))
-	$SFX.play(0)
+	$SFX.play()
 
 func _process(delta):
 	translate(delta * velocity)
