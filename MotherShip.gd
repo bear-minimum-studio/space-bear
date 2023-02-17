3b7a900c26d70746f3db795c0b1ca2e5eba34971
zelv <contact@elv-studio.com>
2023-02-17 16:16:32 +0100

    feat: add steroid field level + mothership

---
+++ MotherShip.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+var health = 2
+var SPEED = 30
+
+
+func _physics_process(delta):
+	velocity = Vector2(SPEED, 0).rotated(-PI/4.5); # up right
+	move_and_slide()
+
+func on_hit():
+	if health > 0:
+		health -= 1
+		print("mothership health: ",health)
+	elif health == 0:
+		print("Mothership Ded")
+
+func _on_hit_box_body_entered(body):
+	if body.has_method("on_hit"):
+		body.on_hit()
