b574f682b0867f81305298976ab12c824448b858
Pierre Poupin <pierrep@bam.tech>
2023-02-20 19:05:17 +0100

    wip

--- grappling-hook/GrapplingHookThrowable.gd
+++ grappling-hook/GrapplingHookThrowable.gd
@@ -1,6 +1,7 @@
 
 extends Node2D
 
+const FRAGMENT_SIZE = 10
 const MAX_LENGTH = 5
 const GRAPPLING_THROWING_FORCE = 500
 const GRAPPLING_THROWING_ADJUSTMENT = GRAPPLING_THROWING_FORCE / 2
@@ -14,16 +15,15 @@ var hook_fragment = preload("res://grappling-hook/HookFragment.tscn")
 
 @onready var first_fragment = $HookFragment
 @onready var fragments = [$HookFragment]
-var _should_keep_spawning = true
+var _should_keep_spawning = false
 
 func _on_spawn_if_out_of_this_body_exited(body):
 	# TODO attention verifier que c'est bien un enfant de notre scene
 	if !body.is_in_group("hook_fragment"):
 		return
 	
-	if fragments.size() == MAX_LENGTH and _should_keep_spawning:
-		_should_keep_spawning = false
-		throwing_reference.node_b = fragments[fragments.size() - 1].get_path()
+	if fragments.size() == MAX_LENGTH and !_should_keep_spawning:
+		_stop_spawning()
 		return
 	
 	if fragments.size() > MAX_LENGTH:
@@ -34,9 +34,14 @@ func _on_spawn_if_out_of_this_body_exited(body):
 
 func _spawn():
 	var new_fragment = hook_fragment.instantiate()
-	new_fragment.neighbour_fragment = fragments[fragments.size() - 1].get_path()
+	var previous_fragment = fragments[fragments.size() - 1]
+	new_fragment.neighbour_fragment = previous_fragment.get_path()
+#	new_fragment.transform.origin = previous_fragment.transform.origin
+#	new_fragment.transform.origin.x -= FRAGMENT_SIZE
 	fragments.append(new_fragment)
 	self.add_child(new_fragment)
+#	first_fragment.apply_force(GRAPPLING_THROWING_ADJUSTMENT * Vector2.RIGHT)
+
 
 func launch(thrower: CharacterBody2D, thrower_rotation: float):
 	throwing_reference.node_a = thrower.get_path()
@@ -44,7 +49,7 @@ func launch(thrower: CharacterBody2D, thrower_rotation: float):
 
 	$HookFragment.apply_impulse(GRAPPLING_THROWING_FORCE * Vector2.from_angle(thrower_rotation) + thrower.get_real_velocity())
 
-
-func _on_hook_fragment_body_entered(body):
-	print("hit")
-	_should_keep_spawning = false
+func _stop_spawning():
+	print("stop spawning")
+	_should_keep_spawning = true
+	throwing_reference.node_b = fragments[fragments.size() - 1].get_path()
