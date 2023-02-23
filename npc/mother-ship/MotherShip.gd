b6d0ef963afe0fcf8dc76c2b0d4dafbf4b309154
zelv <contact@elv-studio.com>
2023-02-23 19:29:10 +0100

    feat: remove mothership's hit/hurtbox

--- npc/mother-ship/MotherShip.gd
+++ npc/mother-ship/MotherShip.gd
@@ -1,13 +1,1 @@
 extends "res://npc/AbstractShip.gd"
-
-var health = 30
-
-func on_hit():
-	if health > 0:
-		health -= 1
-	if health <= 0:
-		self.queue_free()
-
-func _on_hit_box_body_entered(body):
-	if body.has_method("on_hit"):
-		body.on_hit()
