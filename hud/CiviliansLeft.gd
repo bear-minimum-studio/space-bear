35e6706ee481f3ee91a9a8617900c1a9ec4da52b
zelv <contact@elv-studio.com>
2023-02-23 21:46:11 +0100

    refactor: lift civilian counting responsibility to Game.gd

--- hud/CiviliansLeft.gd
+++ hud/CiviliansLeft.gd
@@ -1,15 +1,4 @@
 extends Label
 
-var nb_of_civilians
-func _ready():
-	Events.dead_ship.connect(_on_dead_civilian)
-	nb_of_civilians = get_tree().get_nodes_in_group("flock").size()
-	_update_text()
-
-func _on_dead_civilian(dead_ship: Node2D):
-	if dead_ship.is_in_group("flock"):
-		nb_of_civilians = get_tree().get_nodes_in_group("flock").size() - 1
-		_update_text()
-
-func _update_text():
+func update_text(nb_of_civilians : int):
 	self.text = "%s civilian ships left" % nb_of_civilians
