806e865d138dc25d74c85f30023ceaf88e02e4f2
zelv <contact@elv-studio.com>
2023-02-23 21:48:15 +0100

    refactor: clean civilians counting

--- Game.gd
+++ Game.gd
@@ -2,20 +2,18 @@ extends Node2D
 
 @onready var civilians_left = $CanvasLayer/CiviliansLeft
 
-var nb_of_civilians
-
 func _ready():
 	Events.dead_ship.connect(_on_dead_civilian)
-	nb_of_civilians = _count_civilians()
-	_update_text(nb_of_civilians)
+	_update_text(_count_civilians())
 
 func _on_dead_civilian(dead_ship: Node2D):
 	if dead_ship.is_in_group("flock"):
-		nb_of_civilians = _count_civilians() - 1
-		_update_text(nb_of_civilians)
+		# The civilian is freed AFTER the signal is sent. So, it won't be counted as dead when we receive the signal.
+		var nb_of_civilians_left = _count_civilians() - 1
+		_update_text(nb_of_civilians_left)
 
 func _count_civilians():
 	return get_tree().get_nodes_in_group("flock").size()
 
-func _update_text(nb_of_civilians: int):
-	civilians_left.update_text(nb_of_civilians)
+func _update_text(nb_of_civilians_left: int):
+	civilians_left.update_text(nb_of_civilians_left)
