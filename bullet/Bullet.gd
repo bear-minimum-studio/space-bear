9f4ae1894326b44e222a3a2e4b7c6e6251645f74
alexandre.bobet <alexandre.bobet@ezygain.com>
2023-02-26 20:15:31 +0100

    feat: add bullet max range

--- bullet/Bullet.gd
+++ bullet/Bullet.gd
@@ -1,16 +1,23 @@
 extends Area2D
-
 class_name Bullet
 
+@export var bullet_range = 10000
+
+# Exported value, do not rename
+# TODO: make this internal
 const BULLET_SPEED = 500
 
 var velocity = Vector2.ZERO;
+var initial_position = null
 
 func _ready():
 	$SFX.play()
+	initial_position = self.global_position
 
 func _process(delta):
 	translate(delta * velocity)
+	if (self.global_position - initial_position).length() > bullet_range:
+		self.queue_free()
 
 
 func _on_body_entered(body: Node2D):
