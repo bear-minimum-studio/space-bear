7bdf1b419c63bd42eb94c89612aaae34d46a0caf
alexandre.bobet <alexandre.bobet@ezygain.com>
2023-02-26 17:24:15 +0100

    feat: add player radar

---
+++ hud/radar/PlayerRadar.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+var radar_element_scene = preload("res://hud/radar/RadarElement.tscn")
+
+func _physics_process(delta):
+	self.global_rotation = 0
+
+	var enemies = get_tree().get_nodes_in_group("enemy")
+	_sync_children(enemies.size())
+	
+	var index = 0
+	for enemy in enemies:
+		self.get_child(index).rotation = (enemy.global_position - self.global_position).angle()
+		index += 1
+	
+func _sync_children(nb_enemies: int):
+	var nb_children = self.get_child_count()
+	if nb_children == nb_enemies:
+		return
+	
+	if nb_children > nb_enemies:
+		var nb_children_to_free = nb_children - nb_enemies
+		for i in range(nb_children_to_free):
+			var child_to_free = get_child(i)
+			child_to_free.queue_free()
+	
+	if nb_enemies > nb_children:
+		var nb_children_to_create = nb_enemies - nb_children
+		for i in range(nb_children_to_create):
+			var new_child = radar_element_scene.instantiate()
+			self.add_child(new_child)
