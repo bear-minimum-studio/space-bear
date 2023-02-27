da4fffe3be01bea0906a9c5e40f48c0bb13c8bc0
Pierre Poupin <pierrep@bam.tech>
2023-02-27 17:26:16 +0100

    feat: prettier health bars with animations

--- npc/ShipHealth.gd
+++ npc/ShipHealth.gd
@@ -4,13 +4,15 @@ extends TextureProgressBar
 
 var timer: SceneTreeTimer = null
 const DISPLAY_DURATION = 2
+const ANIMATION_IN_DURATION = 0.2
+const ANIMATION_OUT_DURATION = 1
 
 func _ready():
-	self.visible = false
+	_toggle_visibility(false, true)
 	health_system.hp_changed.connect(_on_hp_changed)
 
 func _on_hp_changed(health, max_health):
-	self.visible = true
+	_toggle_visibility(true)
 	self.max_value = max_health
 	self.value = health
 
@@ -21,4 +23,14 @@ func _on_hp_changed(health, max_health):
 	
 	await timer.timeout
 	timer = null
-	self.visible = false
+	_toggle_visibility(false)
+
+func _toggle_visibility(new_visibility: bool, disable_animation = false):
+	if disable_animation:
+		self.modulate.a = 1 if new_visibility == true else 0
+		return
+
+	if new_visibility == true:
+		create_tween().tween_property(self, "modulate:a", 1, ANIMATION_IN_DURATION)
+	else:
+		create_tween().tween_property(self, "modulate:a", 0, ANIMATION_OUT_DURATION)
