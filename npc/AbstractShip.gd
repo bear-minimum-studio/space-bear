6ef079a904a0bcecb9d56edb15ef924c6b851f89
alexandre.bobet <alexandre.bobet@ezygain.com>
2023-02-28 02:20:58 +0100

    refacto: AbstractShip more readable

--- npc/AbstractShip.gd
+++ npc/AbstractShip.gd
@@ -10,25 +10,27 @@ extends CharacterBody2D
 @onready var hurt_animation = $HurtAnimation
 @onready var health_bar = $UI/HealthBar
 
-var _next_path_position : Vector2
-var _current_agent_position : Vector2
-
 var movement_target: Vector2
 
-func _physics_process(_delta):
-	if nav_agent.is_navigation_finished():
-		return
+func _ready():
+	nav_agent.set_target_desired_distance(30.0)
 
-	_next_path_position = nav_agent.get_next_path_position()
-	_current_agent_position = global_transform.origin
-	velocity = (_next_path_position - _current_agent_position).normalized() * speed
-	self.rotation = velocity.angle()
+func _physics_process(_delta):
+	_set_velocity()
 	move_and_slide()
+
+func _set_velocity():
+	if nav_agent.is_navigation_finished():
+		velocity = Vector2.ZERO
+	else:
+		var next_path_position = nav_agent.get_next_path_position()
+		var direction = (next_path_position - global_position).normalized()
+		velocity = direction * speed
+		rotation = velocity.angle()
 
 func set_movement_target(new_movement_target : Vector2):
 	movement_target = new_movement_target
 	nav_agent.set_target_position(movement_target)
-
 
 func _on_health_system_dead():
 	Events.dead_ship.emit(self)
