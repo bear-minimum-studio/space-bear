e75b6611a287148f76a0898a6230f82d4e83765d
zelv <contact@elv-studio.com>
2023-02-28 14:37:22 +0100

    feat: multiple civilians per ship

--- Game.gd
+++ Game.gd
@@ -6,14 +6,17 @@ func _ready():
 	Events.dead_ship.connect(_on_dead_civilian)
 	_update_text(_count_civilians())
 
-func _on_dead_civilian(dead_ship: Node2D):
+func _on_dead_civilian(dead_ship: AbstractCivilianShip):
 	if dead_ship.is_in_group("flock"):
 		# The civilian is freed AFTER the signal is sent. So, it won't be counted as dead when we receive the signal.
-		var nb_of_civilians_left = _count_civilians() - 1
+		var nb_of_civilians_left = _count_civilians() - dead_ship.number_of_passengers
 		_update_text(nb_of_civilians_left)
 
 func _count_civilians():
-	return get_tree().get_nodes_in_group("flock").size()
+	var number_of_civilians = 0
+	for ship in get_tree().get_nodes_in_group("flock"):
+		number_of_civilians += ship.number_of_passengers
+	return number_of_civilians
 
 func _update_text(nb_of_civilians_left: int):
 	civilians_left.update_text(nb_of_civilians_left)
--- npc/civilians/AbstractCivilianShip.gd
+++ npc/civilians/AbstractCivilianShip.gd
@@ -1,3 +1,4 @@
 extends "res://npc/AbstractShip.gd"
+class_name AbstractCivilianShip
 
 @export_range(0,10000,1,"or_greater") var number_of_passengers = 0;
