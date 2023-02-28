005079a9717377cd0e583ce920a15bd79be4dff1
alexandre.bobet <alexandre.bobet@ezygain.com>
2023-02-28 17:04:42 +0100

    fix: Make turret range different for all turrets

--- npc/Turret.gd
+++ npc/Turret.gd
@@ -3,7 +3,7 @@ extends Area2D
 
 @onready var collision_shape_2d = $CollisionShape2D
 
-@export_range(0.0, 500.0, 10.0, "or_greater") var turret_range: float = 100.0 :
+@export_range(0.0, 500.0, 10.0, "or_greater") var turret_range: float = 250.0 :
 	set(new_turret_range):
 		turret_range = new_turret_range
 		if collision_shape_2d != null:
