743bebff7047288169832029eb47f186c2663309
Pierre Poupin <pierrep@bam.tech>
2023-03-01 16:05:54 +0100

    fix: fix shooting not working

--- World.gd
+++ World.gd
@@ -4,4 +4,5 @@ extends "res://GenericWorld.gd"
 @onready var wormhole = $Wormhole
 
 func _ready():
+	super._ready()
 	get_tree().call_group("flock", "set_convoy_path", wormhole.global_position, mother_ship.global_position)
