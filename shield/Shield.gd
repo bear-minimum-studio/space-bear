7eada7580088780d306ace69cddd9091c3bfaa2f
zelv <contact@elv-studio.com>
2023-03-02 16:13:43 +0100

    feat: add shield shader animation

--- shield/Shield.gd
+++ shield/Shield.gd
@@ -9,6 +9,7 @@ const COLLISION_MARGIN = 10
 @onready var health_system = $HealthSystem
 @onready var old_collision_layer = collision_layer
 @export_range(1.0, 30.0) var recharge_delay_seconds = 5.0
+@onready var sprite_2d = $Sprite2D
 
 @export var max_health = 20 :
 	set(new_max_health):
@@ -18,53 +19,63 @@ const COLLISION_MARGIN = 10
 @export var shield_color: Color = Color("#0096ff2b") :
 	set(new_color):
 		shield_color = new_color
-		queue_redraw()
+		_update_shield_color()
 
-@export var shield_outline: Color = Color("#0974ad") :
-	set(new_color):
-		shield_outline = new_color
-		queue_redraw()
 
 @export var shield_size: float = 100.0 :
 	set(new_shield_size):
 		shield_size = new_shield_size
 		_update_collision_shape_margin()
-		queue_redraw()
+		_update_shield_sprite_size()
 
-@export var outline_width: float = 2.0 :
-	set(new_outline_width):
-		outline_width = new_outline_width
-		queue_redraw()
 
 var timer_before_heal: SceneTreeTimer = null
 
 func _ready():
 	_update_collision_shape_margin()
+	_update_shield_sprite_size()
+	_update_shield_color()
 	if Engine.is_editor_hint():
 		return
 	health_system.hp_changed.connect(_on_hit)
 	health_system.dead.connect(_on_dead)
 	_initialize_max_health()
 
-func _draw():
-	draw_circle(Vector2.ZERO, shield_size, shield_color)
-	draw_arc(Vector2.ZERO, shield_size, 0, 2 * PI, 50, shield_outline, outline_width, false)
+func _update_shield_color():
+	if sprite_2d != null:
+		var opaque_color = shield_color
+		opaque_color.a = 1
+		sprite_2d.material.set_shader_parameter('color', opaque_color)
 
 func _on_hit(_health, _max_health):
-	hurt_animation.animate_hurt_opacity(self)
-	
 	if timer_before_heal == null:
 		timer_before_heal = self.get_tree().create_timer(recharge_delay_seconds)
 		await timer_before_heal.timeout
 		timer_before_heal = null
 		health_system.heal_to_max()
-		return
 	else:
 		timer_before_heal.time_left = recharge_delay_seconds
+
+	var hit_increase = 0.17
+	var current_intensity = sprite_2d.material.get_shader_parameter('hit_intensity');
+	var hit_intensity = min(1.0,current_intensity+hit_increase)
+	var tween_health = create_tween()
+	tween_health.tween_property(sprite_2d, "material:shader_parameter/hit_intensity",hit_intensity,0.1)
+	tween_health.tween_property(sprite_2d, "material:shader_parameter/hit_intensity",0.0,0.3)
+	
+	var health_amount = (_health / float(_max_health))
+	var tween_hit = create_tween()
+	tween_hit.tween_property(sprite_2d, "material:shader_parameter/opacity",health_amount,0.05)
+
 
 func _update_collision_shape_margin():
 	if collision_shape_2d != null:
 		collision_shape_2d.shape.radius = shield_size + COLLISION_MARGIN
+
+func _update_shield_sprite_size():
+	if sprite_2d != null:
+		var sprite_scale = shield_size / 100.0 # TODO: use default size instead (100 right now)
+		sprite_2d.apply_scale(Vector2(sprite_scale,sprite_scale))
 
 func _initialize_max_health():
 	if health_system != null:
