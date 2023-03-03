8d60c2b54a79149171ddfa908268c4a61c54e3f2
Pierre Poupin <pierrep@bam.tech>
2023-03-03 20:13:50 +0100

    refactor: take hook logic out of the world

--- AbstractWorld.gd
+++ AbstractWorld.gd
@@ -1,33 +1,12 @@
 extends Node2D
 
-@onready var player = $Player
 @onready var flock = $Flock
-
-var grappling_hook_scene = preload("res://grappling-hook/GrapplingHook.tscn")
-var current_hook : Node2D
 
 func _ready():
 	WorldReference.current_world = self
 	
 	Events.convoy_reached_wormhole.connect(_on_convoy_reached_wormhole)
 	Events.player_reached_wormhole.connect(_on_player_reached_wormhole)
-
-func _on_player_shoot_grappling_hook(global_player_position, global_player_rotation):
-	# If shooting again while a grappling hook exists, delete the current one
-	if current_hook != null:
-		current_hook.queue_free()
-		current_hook = null
-		return
-
-	var new_hook = grappling_hook_scene.instantiate()
-	
-	new_hook.global_position = global_player_position
-	new_hook.global_rotation = global_player_rotation
-
-	self.add_child(new_hook)
-
-	current_hook = new_hook
-	new_hook.launch(player, global_player_rotation)
 
 
 func _on_convoy_reached_wormhole():
--- player/TurretControl.gd
+++ player/TurretControl.gd
@@ -7,8 +7,11 @@ const ROTATION_SPEED = 2.0
 const HOOK_COOLDOWN = 1.0
 
 var _reloading_hook = false
+var current_hook: Node2D
+var grappling_hook_scene = preload("res://grappling-hook/GrapplingHook.tscn")
 
 @onready var p_2_nozzle = $P2Nozzle
+@export var player: Node2D
 
 func _physics_process(delta):
 	var rotation_intensity = Input.get_axis("turret_left", "turret_right")
@@ -21,8 +24,25 @@ func _shoot_hook():
 	if _reloading_hook:
 		return
 
-	nozzle_shoot_grappling_hook.emit(p_2_nozzle.global_position, self.global_rotation)
+	_do_shoot_hook(p_2_nozzle.global_position, self.global_rotation)
 	
 	_reloading_hook = true
 	await get_tree().create_timer(HOOK_COOLDOWN).timeout
 	_reloading_hook = false
+
+func _do_shoot_hook(global_player_position, global_player_rotation):
+	# If shooting again while a grappling hook exists, delete the current one
+	if current_hook != null:
+		current_hook.queue_free()
+		current_hook = null
+		return
+
+	var new_hook = grappling_hook_scene.instantiate()
+	
+	new_hook.global_position = global_player_position
+	new_hook.global_rotation = global_player_rotation
+
+	WorldReference.current_world.add_child(new_hook)
+
+	current_hook = new_hook
+	new_hook.launch(player, global_player_rotation)
