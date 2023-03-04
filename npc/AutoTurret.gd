cadf81577849b4fbc9bffe93ed8e37849e71943a
Pierre Poupin <pierrep@bam.tech>
2023-03-04 22:37:28 +0100

    fix: wrong range for turrets

--- npc/AutoTurret.gd
+++ npc/AutoTurret.gd
@@ -4,9 +4,7 @@ extends "res://npc/Turret.gd"
 @export_range(0.0, 500.0, 10.0, "or_greater") var turret_range: float = 250.0 :
 	set(new_turret_range):
 		turret_range = new_turret_range
-		if collision_shape_2d != null:
-			collision_shape_2d.shape.radius = turret_range
-		queue_redraw()
+		_update_range()
 
 const turret_target_mapping = {
 	TurretType.ENEMY: "ally",
@@ -14,6 +12,15 @@ const turret_target_mapping = {
 }
 
 var target : Node2D = null
+
+func _ready():
+	_update_range()
+
+func _update_range():
+	if collision_shape_2d == null:
+		return
+
+	collision_shape_2d.shape.radius = turret_range
 
 func _physics_process(delta):
 	if Engine.is_editor_hint():
