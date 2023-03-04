ad4766f78e572e12177561050f579ff123e3e659
Pierre Poupin <pierrep@bam.tech>
2023-03-05 00:33:13 +0100

    refactor: rename range to laser_range

--- npc/LaserTurret.gd
+++ npc/LaserTurret.gd
@@ -1,12 +1,12 @@
 extends "res://npc/AbstractTurret.gd"
 
-@export var range = 300.0
+@export var laser_range = 300.0
 
 var laser_scene = preload("res://weapons/laser/Laser.tscn")
 
 func _on_shoot():
 	var new_laser = laser_scene.instantiate()
-	new_laser.range = range
+	new_laser.laser_range = laser_range
 	new_laser.global_rotation = self.global_rotation
 	new_laser.global_position = self.global_position
 	WorldReference.current_world.add_child(new_laser)
