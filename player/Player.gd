6bb3a5e2fb6b755dd704caaa6311c5165b20af08
zelv <contact@elv-studio.com>
2023-03-05 16:19:36 +0100

    feat: move player with zqsd
    in the screen plane:
    z -> up
    q -> left
    s -> down
    d -> right
    
    rotation of the player is handled relatively to the ship with left/right
    arrows

--- player/Player.gd
+++ player/Player.gd
@@ -7,6 +7,10 @@ signal shoot_grappling_hook
 @export_range(0.0,3.0,0.05,"or_greater") var time_to_max_speed = 1.0
 @export_range(0.0,3.0,0.05,"or_greater") var time_to_stop = 0.7
 @export_range(0.0,4.0,0.1,"or_greater") var rotation_per_second = 0.7
+@export_range(0.0,0.3,0.01) var rotation_dead_zone = 0.1
+@export_range(0.01,1.5) var rotation_damping_zone = 0.5
+@export var mouse = true
+@export var remove_inertia = true
 
 @onready var rotation_speed = 2 * PI * rotation_per_second
 
@@ -16,7 +20,8 @@ var thrust_intensity :
 var brake_intensity:
 	get: return mass * max_speed / time_to_stop
 
-@onready var flammes = $Flammes
+var current_direction: Vector2 = Vector2.ZERO
+
 @onready var sfx = $SFX
 @onready var turret = $Turrets/Turret
 @onready var p2_turret_control = $P2Turret/P2TurretControl
@@ -45,24 +50,72 @@ func _integrate_forces(state):
 	else:
 		_thrust(state)
 	_custom_set_rotation()
-		
+
 func _thrust(state):
-	var intensity = Input.get_axis("decelerate", "accelerate")
-	var thrust = intensity * thrust_intensity * Vector2.from_angle(rotation)
+	var direction = Input.get_vector("go_left", "go_right", "go_up", "go_down")
+	var thrust = thrust_intensity * direction
 	if linear_velocity.length() >= max_speed:
 		# maintain max speed but change direction
 		thrust = thrust - (linear_velocity.normalized() * thrust.length())
 	state.apply_central_force(thrust)
-	if intensity:
-		flammes.visible = true
+	if remove_inertia:
+		_compensate_inertia(state)
+	
+	var possible_flammes = {$FlammesDown: [0.0], $FlammesRight: [-PI/2], $FlammesUp: [-PI,PI], $FlammesLeft: [PI/2]}
+	if direction.length():
+		var a = _normalize_angle(direction.angle() - self.rotation)
+		var alpha = 5 * PI / 9
+		for f in possible_flammes:
+			f.visible = false
+			for flamme_angle in possible_flammes[f]:
+				if (a - alpha/2 <= flamme_angle) and (flamme_angle <= a + alpha/2):
+					f.visible = true
 		sfx.play()
 	else:
-		flammes.visible = false
+		for f in possible_flammes:
+			f.visible = false
 		sfx.stop()
-	
+
+func _compensate_inertia(state):
+	var direction = Input.get_vector("go_left", "go_right", "go_up", "go_down")
+	if direction.length():
+		var orthonormal_velocity = linear_velocity.project(direction.orthogonal())
+		var thrust = - thrust_intensity * orthonormal_velocity / max_speed
+		if thrust:
+			state.apply_central_force(thrust)
+
+## return a scaling coefficient depending on how much the ship has to rotate
+## In dead zone -> no rotation
+# in damping zone -> slow rotation
+# out of damping zone -> scale with angle of rotation
+func _rotation_damping(remainder_angle: float) -> float:
+	var angle = abs(remainder_angle)
+	if (not mouse) and (angle < rotation_dead_zone):
+		return 0
+	if angle < rotation_damping_zone:
+		return 1 - (rotation_damping_zone - angle) / rotation_damping_zone
+	return 1 + (angle - rotation_damping_zone) / PI
+
+## Normalize angle between -PI and PI
+func _normalize_angle(angle: float):
+	return fposmod(angle + PI, 2*PI) - PI
+
 func _torque():
-	var intensity = Input.get_axis("turn_left", "turn_right")
-	angular_velocity = rotation_speed * intensity
+	var intensity = 0
+	if mouse:
+		intensity = 1
+		current_direction = get_viewport().get_mouse_position() - Vector2(240,135)
+	else:
+		var direction = Input.get_vector("look_left", "look_right", "look_up", "look_down")
+		intensity = direction.length()
+		if intensity > 0:
+			current_direction = direction
+
+	var remainder_angle = _normalize_angle(current_direction.angle() - self.global_rotation)
+	var rotation_sign = sign(remainder_angle)
+	print('current: ', current_direction.angle(), '	self: ', self.global_rotation, ' modulo: ', remainder_angle)
+	self.angular_velocity = rotation_sign * rotation_speed * intensity * _rotation_damping(remainder_angle)
+
 
 func _brake(state):
 	if linear_velocity.length() > 10:
