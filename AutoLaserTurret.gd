b1a84bffddd3dda8a050ba0f5cd70736d9a2ee50
alexandre.bobet <alexandre.bobet@ezygain.com>
2023-03-05 02:01:49 +0100

    feat: Add auto laser turrer

---
+++ AutoLaserTurret.gd
@@ -0,0 +1,18 @@
+@tool
+extends "res://npc/LaserTurret.gd"
+
+@export_range(0.0, 500.0, 10.0, "or_greater") var turret_range: float = 250.0 :
+	set(new_turret_range):
+		turret_range = new_turret_range
+		_update_range()
+
+@onready var collision_shape_2d = $CollisionShape2D
+
+func _ready():
+	_update_range()
+
+func _update_range():
+	if collision_shape_2d == null:
+		return
+
+	collision_shape_2d.shape.radius = turret_range
