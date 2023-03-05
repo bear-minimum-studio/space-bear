518e5ae62744d3d5f7fcfdf78d1572308740e076
Pierre Poupin <pierrep@bam.tech>
2023-03-05 02:26:44 +0100

    fix: adjust laser nozzle

--- npc/LaserTurret.gd
+++ npc/LaserTurret.gd
@@ -8,5 +8,5 @@ func _on_shoot():
 	var new_laser = laser_scene.instantiate()
 	new_laser.laser_range = laser_range
 	new_laser.global_rotation = self.global_rotation
-	new_laser.global_position = self.global_position
+	new_laser.global_position = nozzle.global_position
 	WorldReference.current_world.add_child(new_laser)
