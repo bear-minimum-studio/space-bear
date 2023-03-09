f60ffdc1676ae86723ca3d1e896e158d8a3da38a
Pierre Poupin <pierrep@bam.tech>
2023-03-09 17:16:05 +0100

    feat: add first level along with some balance proposal

--- npc/enemies/VultureEnemy.gd
+++ npc/enemies/VultureEnemy.gd
@@ -1,5 +1,7 @@
 extends "res://npc/enemies/AbstractEnemy.gd"
 
+# TODO this whole script is copy pasted for each enemy
+
 @export_range(0.0,2.0,0.05,"or_greater") var proximity_ratio = 0.7
 
 @onready var _turret_range = $Turrets/AutoBulltetTurret.turret_range
