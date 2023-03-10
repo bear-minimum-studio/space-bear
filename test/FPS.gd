54a53ebc6296caa8298c37d7d4e1f84773798bd6
Pierre Poupin <pierrep@bam.tech>
2023-03-10 17:02:22 +0100

    fix: fix warnings

--- test/FPS.gd
+++ test/FPS.gd
@@ -4,5 +4,5 @@ func _ready():
 	if not OS.is_debug_build():
 		self.visible = false
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	set_text("FPS %s" % Engine.get_frames_per_second())
--- weapons/projectiles/laser/HealingLaser.gd
+++ weapons/projectiles/laser/HealingLaser.gd
@@ -1,4 +1,4 @@
 extends "res://weapons/projectiles/laser/Laser.gd"
 
 func _hit(area_or_body):
-	var health_system = HealthSystem.heal_health_system(area_or_body, damage_or_heal)
+	HealthSystem.heal_health_system(area_or_body, damage_or_heal)
