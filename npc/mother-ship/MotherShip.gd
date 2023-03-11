9e24fee3cd1ab9852a6b9a28539357b36b67dafe
Pierre Poupin <pierrep@bam.tech>
2023-03-11 19:29:45 +0100

    fix: fix mothership missing animation

--- npc/mother-ship/MotherShip.gd
+++ npc/mother-ship/MotherShip.gd
@@ -2,5 +2,6 @@ extends "res://npc/civilians/AbstractCivilianShip.gd"
 
 class_name MotherShip
 
-func _on_health_system_hp_changed(health, max_health, _difference):
+func _on_health_system_hp_changed(health, max_health, difference):
+	super(health, max_health, difference)
 	Events.mothership_hp_changed.emit(health, max_health)
