8a3690d37de13c8e79c5027172a535388fc64958
Pierre Poupin <pierrep@bam.tech>
2023-03-13 14:36:05 +0100

    refactor: move wheel display logic to ShipUpgradeHandler

--- singletons/ShipUpgradeHandler.gd
+++ singletons/ShipUpgradeHandler.gd
@@ -31,3 +31,34 @@ func _upgrade(ship):
 
 func _on_selected_ship_changed(new_ship):
 	selected_ship = new_ship
+
+
+@onready var ship_upgrade_wheel = $SelectionWheel
+
+var saved_mouse_position: Vector2
+
+func _unhandled_input(event):
+	if event.is_action_pressed("upgrade"):
+		# TODO separate this logic from display
+		if not self.is_ship_selected:
+			return
+
+		process_mode = Node.PROCESS_MODE_ALWAYS
+		get_tree().paused = true
+		ship_upgrade_wheel.show()
+	if event.is_action_released("upgrade"):
+		get_tree().paused = false
+		process_mode = Node.PROCESS_MODE_INHERIT
+
+		# TODO separate this logic from display
+		self.upgrade_candidate = ship_upgrade_wheel.wheel.selection
+		ship_upgrade_wheel.hide()
+
+func _on_visibility_changed():
+	if visible:
+		saved_mouse_position = get_viewport().get_mouse_position()
+		Helpers.set_mouse_position(self, Helpers.get_screen_center(self))
+		ship_upgrade_wheel.wheel.process_mode = Node.PROCESS_MODE_ALWAYS
+	else:
+		Helpers.set_mouse_position(self, saved_mouse_position)
+		ship_upgrade_wheel.wheel.process_mode = Node.PROCESS_MODE_DISABLED
--- ui/selection-wheel/SelectionWheel.gd
+++ ui/selection-wheel/SelectionWheel.gd
@@ -1,31 +1,3 @@
 extends Control
 
-# TODO this script will go into the parent
-
 @onready var wheel = $Wheel
-
-var saved_mouse_position: Vector2
-
-func _unhandled_input(event):
-	if event.is_action_pressed("upgrade"):
-		# TODO get rid of this coupling
-		if not get_parent().is_ship_selected:
-			return
-		process_mode = Node.PROCESS_MODE_ALWAYS
-		get_tree().paused = true
-		self.show()
-	if event.is_action_released("upgrade"):
-		get_tree().paused = false
-		process_mode = Node.PROCESS_MODE_INHERIT
-		# TODO get rid of this coupling
-		get_parent().upgrade_candidate = wheel.selection
-		self.hide()
-
-func _on_visibility_changed():
-	if visible:
-		saved_mouse_position = get_viewport().get_mouse_position()
-		Helpers.set_mouse_position(self, Helpers.get_screen_center(self))
-		wheel.process_mode = Node.PROCESS_MODE_ALWAYS
-	else:
-		Helpers.set_mouse_position(self, saved_mouse_position)
-		wheel.process_mode = Node.PROCESS_MODE_DISABLED
