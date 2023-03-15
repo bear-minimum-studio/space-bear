978172b1f50f2a8cedfa9dfd49267412bcfee3c4
Pierre Poupin <pierrep@bam.tech>
2023-03-15 10:57:59 +0100

    fix: fix number of passengers

--- npc/civilians/AbstractCivilianShip.gd
+++ npc/civilians/AbstractCivilianShip.gd
@@ -1,7 +1,7 @@
 extends "res://npc/AbstractShip.gd"
 class_name AbstractCivilianShip
 
-@export_range(0,10000,1,"or_greater") var number_of_passengers = 0;
+@export_range(0,10000,1,"or_greater") var number_of_passengers = 10;
 @onready var construction_particles = $ConstructionParticles
 @export var ship_model = ShipCatalog.ShipModels.CIVILIAN
 @export var is_ship_upgradable = true
